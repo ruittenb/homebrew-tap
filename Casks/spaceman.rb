b6dba95a591cb86186a9858c1f178a6d0548459c
René Uittenbogaard <ruittenb@gmail.com>
2025-09-26 00:11:26 +0200

    Version 1.12.0

--- Casks/spaceman.rb
+++ Casks/spaceman.rb
@@ -1,6 +1,6 @@
 cask "spaceman" do
-  version "1.11.3"
-  sha256 "bb5416cc065dbc3a1d0275290bc6c90b062c3e9acb5136b6bebe16cb162dd087"
+  version "1.12.0"
+  sha256 "de7fd2a71fdfcbe8ff0469a9a4e896a75a45fd35eb722b9e285bc0212d488237"
 
   url "https://github.com/ruittenb/Spaceman/releases/download/v#{version}/Spaceman-#{version}.dmg"
   name "Spaceman"
