270d42b1ceab42466cba810cc0d545e9f657b573
René Uittenbogaard <ruittenb@gmail.com>
2026-05-27 17:11:51 +0200

    Version 1.22.2

--- Casks/spaceman.rb
+++ Casks/spaceman.rb
@@ -1,13 +1,13 @@
 cask "spaceman" do
   version "1.21.1-alpha"
-  sha256 "4947bbcc9340450c88771638c48036a4faeee948f979f0bcc4d2aa65134ad15b"
+  sha256 "dee3f570c03a695918d29ff786169904fc4e1f21387e9637bc4e9ed189b8ffb6"
 
   url "https://github.com/ruittenb/Spaceman/releases/download/v#{version}/Spaceman-#{version}.dmg"
   name "Spaceman"
   desc "View and navigate Spaces (Virtual Desktops) from the menu bar"
   homepage "https://github.com/ruittenb/Spaceman"
 
-  depends_on macos: ">= :ventura"
+  depends_on macos: ":ventura"
 
   app "Spaceman.app"
 end
